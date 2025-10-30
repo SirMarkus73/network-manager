355c31aa4ad5684c32c2bce9e0b251ca0273aa15
SirMarkus <marcosbarreroherrero@gmail.com>
2025-10-31 00:43:49 +0100

    ci(release): add semantic-release plugins for git and GitHub integration

--- release.config.cjs
+++ release.config.cjs
@@ -1,3 +1,14 @@
 module.exports = {
 	branches: ["main"],
+	plugins: [
+		[
+			"@semantic-release/git",
+			{
+				assets: ["package.json", "CHANGELOG.md"],
+				message:
+					"chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+			},
+		],
+		"@semantic-release/github",
+	],
 };
