0a35a20ad35d59a5decce5f0c2b7f2a5c2e805d1
SirMarkus <marcosbarreroherrero@gmail.com>
2025-10-31 23:47:43 +0100

    feat(wifi): add watch option to wifi list (--watch or -w)
    #41

--- src/commands/wifi/list/command.ts
+++ src/commands/wifi/list/command.ts
@@ -1,10 +1,10 @@
+import ansi from "ansi-escapes";
 import { createCommand, Option } from "commander";
 import { createSpinner } from "nanospinner";
 import { getWifiConnections } from "@/actions/wifi/list/action.js";
 import { WIFI_CHOICES } from "@/constants/wifi.js";
 import { formatWifiTable } from "@/lib/formatWifiTable.js";
 import type { WifiFieldName } from "@/types/wifi.js";
-
 export const listCommand = createCommand("list");
 
 listCommand.description("List available WiFi connections");
@@ -14,15 +14,55 @@ const fieldOption = new Option("-f, --fields <FIELD...>", "Fields to display")
 	.default(["active", "ssid", "bssid", "signal", "chan"])
 	.choices(WIFI_CHOICES);
 
-listCommand.addOption(fieldOption).action(async (options) => {
-	const fields: WifiFieldName[] = Array.from(options.fields);
+const watchOption = new Option("-w, --watch", "Watch changes");
 
-	const spinner = createSpinner("Fetching WiFi connections...").start();
-	const connections = await getWifiConnections(fields);
+listCommand
+	.addOption(fieldOption)
+	.addOption(watchOption)
+	.action(async (options) => {
+		process.stdout.write("\x1B[2J\x1B[0f");
 
-	spinner.success("Fetched WiFi connections");
+		const fields: WifiFieldName[] = Array.from(options.fields);
+		const spinner = createSpinner("Fetching WiFi connections...").start();
+		const connections = await getWifiConnections(fields);
+		spinner.success("Fetched WiFi connections");
 
-	const table = formatWifiTable(fields, connections);
+		const table = formatWifiTable(fields, connections);
+		process.stdout.write(ansi.clearScreen + table);
 
-	console.log(table);
-});
+		if (!options.watch) {
+			return;
+		}
+
+		// Configure stdin to listen for 'q' keypress to exit
+		process.stdin.setRawMode(true);
+		process.stdin.setEncoding("utf8");
+		process.stdin.resume();
+
+		process.stdin.on("data", (key: string) => {
+			if (key.toLowerCase() === "q" || key === "\u0003") {
+				process.stdout.write(ansi.cursorShow);
+				process.stdin.setRawMode(false);
+				process.stdin.pause();
+				process.exit(0);
+			}
+		});
+
+		// Hide the cursor
+		process.stdout.write(ansi.cursorHide);
+
+		// Start watching for changes
+		let previous = table;
+
+		setInterval(async () => {
+			const newConnections = await getWifiConnections(fields);
+			const current = formatWifiTable(fields, newConnections);
+
+			if (current !== previous) {
+				previous = current;
+				process.stdout.write(
+					`${ansi.cursorTo(0, 0) + ansi.eraseDown + current}\nPress 'q' to exit the app\n`,
+				);
+			}
+		}, 1000);
+	});
