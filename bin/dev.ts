c69b62f5cf4cbe81f43d5880112c3ceeda509ae2
SirMarkus <marcosbarreroherrero@gmail.com>
2025-11-01 12:33:40 +0100

    chore(package): remove unused dependencies
    #47

--- bin/dev.ts
+++ bin/dev.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env -S node --loader ts-node/esm --disable-warning=ExperimentalWarning
-import "tsconfig-paths/register.js";
 import { execute } from "@oclif/core";
 
 await execute({ development: true, dir: import.meta.url });
